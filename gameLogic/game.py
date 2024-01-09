6033f8c278d904eec633ccd0b4d2cf5ef55de32d
Parth Dundh <french-fri@users.noreply.github.com>
2024-01-09 10:51:35 +0530

    Finishing the incomplete game logic Implimenting the play method Removing the redundant Move class

--- gameLogic/game.py
+++ gameLogic/game.py
@@ -11,33 +11,35 @@ class Response(Enum):
     DRAW = 3
     INVALID_INPUT = 4
 
-class Move:
-    def __init__(self,player: Player,boxNum:int) -> None:
-        self.player = player
-        self.boxNum = boxNum
-
 class Game:
     def __init__(self, board = [0]*9)-> None:
         self.board:list[int] = board
         self.player = Player.X
+        self.gameOver = False
     
     def Reset(self) -> None:
         self.board = [0]*9
-
-    def Play(self, move:Move) -> Response:
-        if self.board[move.boxNum] != 0:
+        self.player = Player.X
+    
+    def Play(self, index: int) -> Response:
+        if index < 0 or index > 8 or self.board[index] != 0:
             return Response.INVALID_INPUT
 
-    def _winCheck(self)-> list[set]:
+        self.board[index] = self.player
+        if self._winCheck():
+            return Response.WINNER_X if self.player == Player.X else Response.Winner_O
+        
+        if self._tieCheck():
+            return Response.DRAW
+
+        self.Player = Player.X if self.Player == Player.O else Player.O    
+        return Response.ONGOING
+
+        
+    def _winCheck(self) -> list[set]:
         wins =[{0,3,6},{1,4,7},{2,5,8},{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6}]
         taken = {i for i,x in enumerate(self.board) if x == self.player}
         return [win for win in wins if win.issubset(taken)]
 
-    def _tieCheck(self,board):
-        return 0 not in board
-
-if __name__ == "__main__":
-    newGame = Game()
-    newGame.Play(Move(Player.X,3))
-    newGame.Reset()
-
+    def _tieCheck(self) -> bool:
+        return 0 not in self.board
