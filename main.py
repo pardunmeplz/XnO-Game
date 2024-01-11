b14bc458f6093ab4f12c7c920b86ec69b2d6a301
Parth Dundh <french-fri@users.noreply.github.com>
2024-01-11 16:09:11 +0530

    Seperating routes in their own files

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, request
-from gameLogic import Game,Player,Response
+from flask import Flask
+from routes.gameRoute import game_bp
+from routes.homeRoute import home_bp
 
 app = Flask(__name__)
-game = None
-name = None
 
-@app.route("/",methods = ["GET", "POST"])
-def homePage():
-    global game, name
-
-    
-    if request.method == "POST":
-        game = Game()
-        name = request.form["nm"]
-        return redirect(url_for("gamePage"))
-
-    return render_template("home.html") 
-    
-    
-@app.route("/game")
-def gamePage():
-    if not game:
-        return redirect(url_for("homePage"))
-
-    board, player, gameOver, winningLine = game.state().values()
-
-    draw = {Player.X:'X',Player.O:'O',0:''}
-    board = [draw[x] for x in board]
-    player = draw[player]
-    
-    return render_template("game.html",message = "Play Game!", board = board, player = player)
-
-
-
-
+app.register_blueprint(home_bp)
+app.register_blueprint(game_bp)
 
 if __name__ == "__main__":
     app.run(debug = True)
